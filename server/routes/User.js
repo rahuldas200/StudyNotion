d5e1ea89fc24abee5bd2692fb3fb98e1d6519b37
rahul <rdas53300@gmail.com>
2024-03-14 18:14:44 +0530

    .

--- server/routes/User.js
+++ server/routes/User.js
@@ -13,7 +13,7 @@ const {
 const {
   resetPasswordToken,
   resetPassword,
-} = require("../controllers/resetPassword")
+} = require("../controllers/ResetPassword")
 
 const { auth } = require("../middleware/auth")
 
