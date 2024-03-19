cc858156c1497241dc9493146cf763a2e0e7db9b
rahul <rdas53300@gmail.com>
2024-03-20 02:39:00 +0530

    .

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Rahul ❤️ </div>
         </div>
       </div>
     </div>
